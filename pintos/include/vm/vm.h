5e3d9f6e3005889a2c83a7584da9fb3bf8859ecd
dolggul2 <dolggul2@gmail.com>
2026-05-11 23:48:23 +0900

    feat: supplemental_page_table 구조체 선언과 초기화 임시 구현

--- pintos/include/vm/vm.h
+++ pintos/include/vm/vm.h
@@ -81,10 +81,13 @@ struct page_operations {
 #define destroy(page) \
 	if ((page)->operations->destroy) (page)->operations->destroy (page)
 
+#include "hash.h"
+
 /* Representation of current process's memory space.
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash hash_table;
 };
 
 #include "threads/thread.h"
