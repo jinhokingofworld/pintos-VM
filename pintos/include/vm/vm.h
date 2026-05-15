cb9c866463b288788dd247939dbd69391fa260b4
dolggul2 <dolggul2@gmail.com>
2026-05-15 17:06:55 +0900

    feat: page 구조체 멤버 elem 추가

--- pintos/include/vm/vm.h
+++ pintos/include/vm/vm.h
@@ -2,6 +2,7 @@
 #define VM_VM_H
 #include <stdbool.h>
 #include "threads/palloc.h"
+#include "hash.h"
 
 enum vm_type {
 	/* page not initialized */
@@ -46,6 +47,7 @@ struct page {
 	struct frame *frame;   /* Back reference for frame */
 
 	/* Your implementation */
+	struct hash_elem elem; /* need to concat with pages in bucket */
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
