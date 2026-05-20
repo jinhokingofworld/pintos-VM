e7c438c2d372fdc4c3171a9698c4c808ca49e245
dolggul2 <dolggul2@gmail.com>
2026-05-20 15:23:29 +0900

    rm: 머지에서 필요없다 판단

--- pintos/include/vm/anon.h
+++ pintos/include/vm/anon.h
@@ -7,13 +7,6 @@ enum vm_type;
 struct anon_page {
 };
 
-struct segment_load_info {
-            struct file *file;  // 불러올 파일
-            off_t offset;       // 읽기 시작할 부분
-            size_t read_bytes;  // 읽을 크기
-            size_t zero_bytes;  // 0으로 채울 크기
-        };
-
 void vm_anon_init (void);
 bool anon_initializer (struct page *page, enum vm_type type, void *kva);
 
